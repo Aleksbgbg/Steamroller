e72c1a2b2ada6558c3f000ddfd4065c71ddd02d4
Aleksbgbg <kozichka01@abv.bg>
2019-02-08 10:52:52 +0000

    [AlignedAllocation] Add separate methods to compute memory alignment.

--- Steamroller/Memory/AlignedAllocation.cpp
+++ Steamroller/Memory/AlignedAllocation.cpp
@@ -5,7 +5,7 @@
 
 namespace sr
 {
-void* AllocateAligned(const uint32 size, const uint32 alignment)
+void* AllocateAligned(const uint32 size, const uint8 alignment)
 {
 	assert(0 < alignment);
 	assert(alignment <= 128);
@@ -17,16 +17,13 @@ void* AllocateAligned(const uint32 size, const uint32 alignment)
 
 	const uintptr_t allocationAddress = reinterpret_cast<uintptr_t>(allocation);
 
-	const uintptr_t misalignment = allocationAddress & (alignment - 1);
-	const ptrdiff_t allocationAdjustment = alignment - misalignment;
+	const MemoryAlignment memoryAlignment = AlignInsertVoid(allocationAddress, alignment);
 
-	const uintptr_t alignedAllocationAddress = allocationAddress + allocationAdjustment;
-
+	assert(memoryAlignment.allocationAdjustment < 256);
 	// Store alignment in the byte immediately below adjusted address so we can free aligned memory correctly
-	assert(allocationAdjustment < 256);
-	reinterpret_cast<uint8*>(alignedAllocationAddress)[-1] = static_cast<uint8>(allocationAdjustment);
+	reinterpret_cast<uint8*>(memoryAlignment.alignedAddress)[-1] = static_cast<uint8>(memoryAlignment.allocationAdjustment);
 
-	return reinterpret_cast<void*>(alignedAllocationAddress);
+	return reinterpret_cast<void*>(memoryAlignment.alignedAddress);
 }
 
 void FreeAligned(const void* const alignedMemory)
@@ -38,5 +35,34 @@ void FreeAligned(const void* const alignedMemory)
 	void* const initialMemory = reinterpret_cast<void*>(initialAllocationAddress);
 
 	free(initialMemory);
+}
+
+MemoryAlignment Align(const uintptr_t address, const uint8 alignment)
+{
+	const uint8 byteWrap = alignment - 1;
+
+	const uintptr_t misalignment = address & byteWrap;
+
+	// & byteWrap ensures that if address is already aligned,
+	// the adjustment is zero
+	const ptrdiff_t allocationAdjustment = (alignment - misalignment) & byteWrap;
+
+	const uintptr_t alignedAllocationAddress = address + allocationAdjustment;
+
+	return MemoryAlignment{ alignedAllocationAddress, allocationAdjustment };
+}
+
+// If memory is already aligned, leave <alignment> empty space at the start of address
+// for extra storage
+MemoryAlignment AlignInsertVoid(const uintptr_t address, const uint8 alignment)
+{
+	const uintptr_t misalignment = address & (alignment - 1);
+
+	// Allocation adjustments will be <alignment> if address already aligned
+	const ptrdiff_t addressAdjustment = alignment - misalignment;
+
+	const uintptr_t allocationAdjustment = address + addressAdjustment;
+
+	return MemoryAlignment{ allocationAdjustment, addressAdjustment };
 }
 }
--- Steamroller/Memory/AlignedAllocation.hpp
+++ Steamroller/Memory/AlignedAllocation.hpp
@@ -4,7 +4,19 @@
 
 namespace sr
 {
-void* AllocateAligned(const uint32 size, const uint32 alignment);
+struct MemoryAlignment
+{
+	uintptr_t alignedAddress;
+	ptrdiff_t allocationAdjustment;
+};
+
+void* AllocateAligned(const uint32 size, const uint8 alignment);
 
 void FreeAligned(const void* const alignedMemory);
+
+MemoryAlignment Align(const uintptr_t address, const uint8 alignment);
+
+// If memory is already aligned, leave <alignment> empty space at the start of address
+// for extra storage
+MemoryAlignment AlignInsertVoid(const uintptr_t address, const uint8 alignment);
 }
